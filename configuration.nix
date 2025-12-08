7ec19bbc7290328c9103398d1d70b529b0dc30e1
r181104 <sten181104@gmail.com>
2025-12-08 22:30:02 +0530

    automated dev commit

--- configuration.nix
+++ configuration.nix
@@ -1,20 +1,27 @@
 {
   config,
-  pkgs,
   modulesPath,
   ...
-}: {
+}: let
+  pkgs = import (fetchTarball {
+    url = "https://channels.nixos.org/nixos-25.11/nixexprs.tar.xz";
+  }) {};
+  unstable = import (fetchTarball {
+    url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+  }) {};
+in {
   imports = [
     "${modulesPath}/virtualisation/amazon-image.nix"
   ];
-  networking.hostName = "remote-nix";
 
+  networking.hostName = "remote-nix";
   environment.systemPackages = with pkgs; [
     curl
     git
     openssl
     qrencode
     wireguard-tools
+    unstable.neovim
   ];
 
   swapDevices = [
@@ -33,7 +40,7 @@
       allowedUDPPorts = [53];
     };
     trustedInterfaces = ["wg0"];
-    firewall.extraCommands = ''
+    extraCommands = ''
       iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o ens5 -j MASQUERADE
     '';
   };
@@ -47,6 +54,7 @@
     externalInterface = "ens5";
     internalInterfaces = ["wg0"];
   };
+
   networking.wireguard.interfaces."wg0" = {
     ips = ["10.8.0.1/24"];
     listenPort = 51820;
@@ -72,18 +80,15 @@
     mutableSettings = true;
     host = "10.8.0.1";
     port = 3000;
-
     settings = {
       dns = {
         bind_host = "10.8.0.1";
         port = 53;
-
         upstream_dns = [
           "https://1.1.1.1/dns-query"
           "https://1.0.0.1/dns-query"
         ];
       };
-
       filtering = {
         protection_enabled = true;
         filtering_enabled = true;
@@ -92,8 +97,8 @@
   };
 
   system.stateVersion = "25.11";
+
   nix.settings.experimental-features = [
     "nix-command"
-    "flakes"
   ];
 }
