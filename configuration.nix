021adbddb5df4234332a50dbd424090a626a5322
r181104 <sten181104@gmail.com>
2025-12-08 22:53:42 +0530

    automated dev commit

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,9 @@
 in {
   imports = [
     "${modulesPath}/virtualisation/amazon-image.nix"
+    # My custom modules
+    ./modules/fastapi.nix
+    ./modules/wireguard.nix
   ];
 
   networking.hostName = "remote-nix";
@@ -54,53 +57,6 @@ in {
 
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = true;
-  };
-
-  networking.nat = {
-    enable = true;
-    externalInterface = "ens5";
-    internalInterfaces = ["wg0"];
-  };
-
-  networking.wireguard.interfaces."wg0" = {
-    ips = ["10.8.0.1/24"];
-    listenPort = 51820;
-    privateKeyFile = "/etc/wireguard/server.key";
-
-    peers = [
-      {
-        # Laptop
-        publicKey = "b3Ujc0svlDZHqypecZMziIYTdG3ECzgamAqnV+Fshjc=";
-        allowedIPs = ["10.8.0.2/32"];
-      }
-      {
-        # Phone
-        publicKey = "P1xY9TkGgB5ZPBMmHKAcQ5lokdycZgOieIC5KiHCUjc=";
-        allowedIPs = ["10.8.0.3/32"];
-      }
-    ];
-  };
-
-  services.adguardhome = {
-    enable = true;
-    openFirewall = false;
-    mutableSettings = true;
-    host = "10.8.0.1";
-    port = 3000;
-    settings = {
-      dns = {
-        bind_host = "10.8.0.1";
-        port = 53;
-        upstream_dns = [
-          "https://1.1.1.1/dns-query"
-          "https://1.0.0.1/dns-query"
-        ];
-      };
-      filtering = {
-        protection_enabled = true;
-        filtering_enabled = true;
-      };
-    };
   };
 
   system.stateVersion = "25.11";
