3371d40ffd583bf276c0d62328b682019d4b9d92
r181104 <sten181104@gmail.com>
2025-12-08 23:24:30 +0530

    automated dev commit

--- configuration.nix
+++ configuration.nix
@@ -18,6 +18,8 @@ in {
   ];
 
   networking.hostName = "remote-nix";
+  environment.shells = with pkgs; [bash];
+
   environment.systemPackages = with pkgs; [
     curl
     git
@@ -59,8 +61,22 @@ in {
     "net.ipv4.ip_forward" = true;
   };
 
-  system.stateVersion = "25.11";
+  time.timeZone = "Asia/Kolkata";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+  };
 
+  system.stateVersion = "25.11";
   nix.settings.experimental-features = [
     "nix-command"
   ];
