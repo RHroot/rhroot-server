cfb6f7647f6c2b894a511b9e00f5dc3ba3932abe
RHroot <sten181104@gmail.com>
2025-12-18 16:46:22 +0530

    automated dev commit

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -6,9 +6,8 @@
   services.nginx = {
     enable = true;
 
-    # Global Cloudflare IP handling
+    # Tell nginx to trust Cloudflare and get real client IP
     appendHttpConfig = ''
-      # Trust Cloudflare IPs and extract real client IP
       real_ip_header CF-Connecting-IP;
 
       # Cloudflare IPv4
@@ -38,7 +37,7 @@
       set_real_ip_from 2c0f:f248::/32;
     '';
 
-    # Main site
+    # Main domain
     virtualHosts."rishabhhaldiya.me" = {
       root = "/var/www/rishabhhaldiya.me";
 
@@ -46,32 +45,10 @@
       forceSSL = true;
 
       extraConfig = ''
-        # Allow ONLY Cloudflare
-        allow 173.245.48.0/20;
-        allow 103.21.244.0/22;
-        allow 103.22.200.0/22;
-        allow 103.31.4.0/22;
-        allow 141.101.64.0/18;
-        allow 108.162.192.0/18;
-        allow 190.93.240.0/20;
-        allow 188.114.96.0/20;
-        allow 197.234.240.0/22;
-        allow 198.41.128.0/17;
-        allow 162.158.0.0/15;
-        allow 104.16.0.0/13;
-        allow 104.24.0.0/14;
-        allow 172.64.0.0/13;
-        allow 131.0.72.0/22;
-
-        allow 2400:cb00::/32;
-        allow 2606:4700::/32;
-        allow 2803:f800::/32;
-        allow 2405:b500::/32;
-        allow 2405:8100::/32;
-        allow 2a06:98c0::/29;
-        allow 2c0f:f248::/32;
-
-        deny all;
+        # Allow only Cloudflare (header-based, correct way)
+        if ($http_cf_connecting_ip = "") {
+          return 403;
+        }
       '';
     };
 
@@ -82,17 +59,21 @@
       enableACME = true;
       forceSSL = true;
 
-      extraConfig = config.services.nginx.virtualHosts."rishabhhaldiya.me".extraConfig;
+      extraConfig = ''
+        if ($http_cf_connecting_ip = "") {
+          return 403;
+        }
+      '';
     };
 
-    # Catch-all: reject IP / unknown hosts
+    # Catch-all: block IP / unknown hosts
     virtualHosts."_" = {
       default = true;
-      rejectSSL = true;
+      return = "444";
     };
   };
 
-  # ACME global config
+  # ACME / Let's Encrypt
   security.acme = {
     acceptTerms = true;
     defaults.email = "rishabhhaldiya.me@proton.me";
