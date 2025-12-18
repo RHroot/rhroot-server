f85717a335ec9821217be19fcf8e3b9b7b300ddf
RHroot <sten181104@gmail.com>
2025-12-18 16:10:11 +0530

    automated dev commit

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -7,17 +7,27 @@
     enable = true;
 
     virtualHosts."rishabhhaldiya.me" = {
+      root = "/var/www/rishabhhaldiya.me";
+
+      addSSL = true;
       forceSSL = true;
+
       sslCertificate = "/etc/ssl/cloudflare/cert.pem";
       sslCertificateKey = "/etc/ssl/cloudflare/key.pem";
-      root = "/var/www/rishabhhaldiya.me";
+
+      locations."/" = {
+        index = "index.html";
+      };
     };
 
     virtualHosts."www.rishabhhaldiya.me" = {
+      root = "/var/www/rishabhhaldiya.me";
+
+      addSSL = true;
       forceSSL = true;
+
       sslCertificate = "/etc/ssl/cloudflare/cert.pem";
       sslCertificateKey = "/etc/ssl/cloudflare/key.pem";
-      root = "/var/www/rishabhhaldiya.me";
     };
   };
 }
